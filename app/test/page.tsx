7c4d36c733a28599d0996cd69e714e59b00f3f37
nayakdhawal <nayakdhawal@gmail.com>
2025-10-05 14:51:01 +0530

    deployment 3

--- app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function TestPage() {
-  return (
-    <div style={{ padding: '20px', backgroundColor: '#1a1a1a', color: '#fff', minHeight: '100vh' }}>
-      <h1>Test Page</h1>
-      <p>If you can see this, the basic Next.js app is working!</p>
-      <p>Time: {new Date().toISOString()}</p>
-    </div>
-  )
-}
